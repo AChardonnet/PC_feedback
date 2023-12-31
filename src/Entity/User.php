149c971b557437d1ae820b91e5a880be1905a717
AChardonnet <antoine@chardonnet.net>
2023-12-31 14:08:54 +0100

    Revert "ajout des champs uniques email et username"
    This reverts commit 3c10f7fd89bb1a12e701f977c29f7e1dc26c354f.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,8 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity('email','username')]
 class User
 {
     #[ORM\Id]
@@ -22,7 +19,6 @@ class User
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
