f66eb4e049526af96ccb83685836bac98b98d234
AChardonnet <antoine@chardonnet.net>
2023-12-31 14:16:56 +0100

    ajout des contraintes pour les champs uniques : email et username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,8 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email')]
+#[UniqueEntity('username')]
 class User
 {
     #[ORM\Id]
