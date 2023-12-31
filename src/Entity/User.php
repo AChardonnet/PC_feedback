3c10f7fd89bb1a12e701f977c29f7e1dc26c354f
AChardonnet <antoine@chardonnet.net>
2023-12-31 14:05:00 +0100

    ajout des champs uniques email et username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,8 +6,11 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email','username')]
 class User
 {
     #[ORM\Id]
@@ -19,6 +22,7 @@ class User
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
