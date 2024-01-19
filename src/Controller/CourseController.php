d005be6abdebccac87342e1da4f19f9e6c7a7b3f
AChardonnet <antoine@chardonnet.net>
2024-01-19 23:37:55 +0100

    création de la route courses permettant de visualiser l'arborescence des cours

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CourseController extends AbstractController
 {
-    #[Route('/course', name: 'app_course')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER')]
+    #[Route('/courses', name: 'app_courses')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
         return $this->render('course/index.html.twig', [
-
+            'courses' => $entityManager->getRepository(Course::class)->findAllAlphabetically()
         ]);
     }
 }
