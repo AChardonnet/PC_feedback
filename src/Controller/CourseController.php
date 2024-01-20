ea7846a22c16c627e04b9e8eb23aa8e56c5d16e8
AChardonnet <antoine@chardonnet.net>
2024-01-20 13:46:11 +0100

    ajout de liens vers les /course/{id} dans /courses

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Course;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,7 +12,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class CourseController extends AbstractController
 {
-    #[Route('/courses', name: 'app_courses')]
+    #[Route('/courses', name: 'course_courses')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         return $this->render('course/index.html.twig', [
@@ -21,17 +20,11 @@ class CourseController extends AbstractController
         ]);
     }
 
-    #[Route('/course/{id}', name: 'app_course')]
+    #[Route('/course/{id}', name: 'course_course')]
     public function course(EntityManagerInterface $entityManager, int $id): Response
     {
         return $this->render('course/course.html.twig', [
             'course' => $entityManager->getRepository(Course::class)->find($id)
         ]);
-    }
-
-    #[Route('/test')]
-    public function test(): RedirectResponse
-    {
-        return $this->redirectToRoute('app_course', ['id' => 2]);
     }
 }
