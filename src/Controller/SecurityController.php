e25299a15a402fc00b1646b1502466ad970adfb2
AChardonnet <antoine@chardonnet.net>
2024-02-02 20:45:54 +0100

    ajout de la connexion via My

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,13 +4,20 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class SecurityController extends AbstractController
 {
@@ -49,6 +56,72 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/loginmy', name: 'security_loginmy')]
+    public function loginMy(Request $request, TokenStorageInterface $tokenStorage, SessionInterface $session, EventDispatcherInterface $dispatcher, EntityManagerInterface $manager)
+    {
+        if($this->isGranted('ROLE_USER')){
+            return $this->redirectToRoute('home_home');
+        }
+
+        $id = $this->getParameter('oauth_id');
+        $secret = $this->getParameter('oauth_secret');
+        $base = $this->getParameter('oauth_base');
+
+        $client = new \OAuth2\Client($id, $secret);
+
+        if(!$request->query->has('code')){
+            $url = $client->getAuthenticationUrl($base.'/oauth/v2/auth', $this->generateUrl('security_login', [],UrlGeneratorInterface::ABSOLUTE_URL));
+            return $this->redirect($url);
+        }
+        else{
+            $params = ['code' => $request->query->get('code'), 'redirect_uri' => $this->generateUrl('security_login', [],UrlGeneratorInterface::ABSOLUTE_URL)];
+            $resp = $client->getAccessToken($base.'/oauth/v2/token', 'authorization_code', $params);
+
+            if(isset($resp['result']) && isset($resp['result']['access_token'])){
+                $info = $resp['result'];
+
+                $client->setAccessTokenType(\OAuth2\Client::ACCESS_TOKEN_BEARER);
+                $client->setAccessToken($info['access_token']);
+                $response = $client->fetch($base.'/api/user/me');
+                $data = $response['result'];
+
+                $username = $data['username'];
+
+                $user = $manager->getRepository(User::class)->findOneBy(['username'=>$username]);
+                if($user === null){ // Création de l'utilisateur s'il n'existe pas
+                    $user = new User;
+                    $user->setUsername($username);
+                    $user->setPassword(sha1(uniqid()));
+                    $user->setEmail($data['email']);
+                    $user->setLastName($data['nom']);
+                    $user->setFirstName($data['prenom']);
+                    $user->setRoles(['ROLE_USER']);
+
+
+                    $manager->persist($user);
+                    $manager->flush();
+                }
+
+                // Connexion effective de l'utilisateur
+                $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+                $tokenStorage->setToken($token);
+
+                $session->set('_security_main', serialize($token));
+
+                $event = new InteractiveLoginEvent($request, $token);
+                $dispatcher->dispatch($event);
+
+            }
+
+            // Redirection vers l'accueil
+            return $this->redirectToRoute('home_home');
+        }
+
+    }
+
+
+
 
     #[Route('/logout', name: 'security_logout')]
     public function logout(): void
