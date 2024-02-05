34345db4ea0a8a7b64c4558db74918f110683981
AChardonnet <antoine@chardonnet.net>
2024-02-05 17:30:39 +0100

    création du form et de l'action pour créer un cours

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Entity\Course;
 use App\Entity\Feedback;
 use App\Entity\User;
+use App\Form\CourseType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,8 +32,22 @@ class AdminController extends AbstractController
     }
 
     #[Route('/addCourse', name: 'admin_addCourse')]
-    public function addCourse()
+    public function addCourse(Request $request, EntityManagerInterface $entityManager, ManagerRegistry $managerRegistry)
     {
-        return $this->render('admin/addCourse.html.twig');
+        $courses = $entityManager->getRepository(Course::class)->findAllAlphabetically();
+        $course = new Course();
+        $form = $this->createForm(CourseType::class, $course);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $managerRegistry->getManager();
+            $manager->persist($course);
+            $manager->flush();
+            return $this->redirectToRoute('admin_addCourse');
+        }
+        return $this->render('admin/addCourse.html.twig', [
+            'form' => $form,
+            'courses' => $courses,
+        ]);
     }
 }
---
+++ src/Form/CourseType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('isUE')
+            ->add('isOptionGroup')
+            ->add('parent', EntityType::class, [
+                'class' => Course::class,
+'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
